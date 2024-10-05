392babe592311d0d10ebefc9166b3310838fd581
Cong Dinh <congdinh2012@hotmail.com>
2024-10-05 18:01:21 +0700

    MVC Web - Step 1: Create base project

---
+++ src/server/mvc/recipeapp/src/main/java/com/congdinh/recipeapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.congdinh.recipeapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RecipeappApplication.class);
+	}
+
+}
