72dd4c149a9784ebd5e4d3c16e213d8d317fc85c
Cong Dinh <congdinh2012@hotmail.com>
2024-10-07 12:57:33 +0700

    API Web Step 2: Convert MVC CategoryController to API Controller

--- src/server/api/src/main/java/com/congdinh/recipeapp/controllers/CategoryController.java
+++ src/server/api/src/main/java/com/congdinh/recipeapp/controllers/CategoryController.java
@@ -2,29 +2,21 @@ package com.congdinh.recipeapp.controllers;
 
 import java.util.UUID;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.data.domain.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.congdinh.recipeapp.dtos.category.CategoryCreateDTO;
-import com.congdinh.recipeapp.dtos.category.CategoryDTO;
-import com.congdinh.recipeapp.dtos.messages.Message;
+import org.springframework.web.bind.annotation.*;
+import com.congdinh.recipeapp.dtos.category.*;
 import com.congdinh.recipeapp.sevices.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
-@Controller
-@RequestMapping("/manage/categories")
+@RestController
+@RequestMapping("/api/categories")
+@Tag(name = "Category", description = "Category API")
 public class CategoryController {
     private final CategoryService categoryService;
 
@@ -33,13 +25,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public String index(
+    @Operation(summary = "Get all categories")
+    @ApiResponse(responseCode = "200", description = "Return all categories")
+    public ResponseEntity<Page<CategoryDTO>> index(
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false, defaultValue = "name") String sortBy, // Xac dinh truong sap xep
             @RequestParam(required = false, defaultValue = "asc") String order, // Xac dinh chieu sap xep
             @RequestParam(required = false, defaultValue = "0") Integer page,
-            @RequestParam(required = false, defaultValue = "10") Integer size,
-            Model model) {
+            @RequestParam(required = false, defaultValue = "10") Integer size) {
         Pageable pageable = null;
 
         if (order.equals("asc")) {
@@ -50,116 +43,64 @@ public class CategoryController {
 
         // Search category by keyword and paging
         var categories = categoryService.findAll(keyword, pageable);
-        model.addAttribute("categories", categories);
 
-        // Passing keyword to view
-        model.addAttribute("keyword", keyword);
-
-        // Passing total pages to view
-        model.addAttribute("totalPages", categories.getTotalPages());
-
-        // Passing total elements to view
-        model.addAttribute("totalElements", categories.getTotalElements());
-
-        // Passing current sortBy to view
-        model.addAttribute("sortBy", sortBy);
-
-        // Passing current order to view
-        model.addAttribute("order", order);
-
-        // Limit page
-        model.addAttribute("pageLimit", 3);
-
-        // Passing current page to view
-        model.addAttribute("page", page);
-
-        // Passing current size to view
-        model.addAttribute("pageSize", size);
-
-        // Passing pageSizes to view
-        model.addAttribute("pageSizes", new Integer[] { 10, 20, 30, 50, 100 });
-
-        // Get message from redirect
-        if (!model.containsAttribute("message")) {
-            model.addAttribute("message", new Message());
-        }
-        return "manage/categories/index";
+        return ResponseEntity.ok(categories);
     }
 
-    @GetMapping("/create")
-    public String create(Model model) {
-        var categoryCreateDTO = new CategoryCreateDTO();
-        model.addAttribute("categoryCreateDTO", categoryCreateDTO);
+    @GetMapping("/{id}")
+    @Operation(summary = "Get category by id")
+    @ApiResponse(responseCode = "200", description = "Return category by id")
+    public ResponseEntity<CategoryDTO> show(@PathVariable UUID id) {
+        var categoryDTO = categoryService.findById(id);
 
-        return "manage/categories/create";
+        return ResponseEntity.ok(categoryDTO);
     }
 
-    @PostMapping("/create")
-    public String create(@Valid @ModelAttribute CategoryCreateDTO categoryCreateDTO,
-            BindingResult bindingResult,
-            RedirectAttributes redirectAttributes,
-            Model model) {
+    @PostMapping()
+    @Operation(summary = "Create new category")
+    @ApiResponse(responseCode = "200", description = "Return new category")
+    @ApiResponse(responseCode = "400", description = "Failed to create category")
+    public ResponseEntity<?> create(@Valid @ModelAttribute CategoryCreateDTO categoryCreateDTO,
+            BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("categoryCreateDTO", categoryCreateDTO);
-            return "manage/categories/create";
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
 
         var result = categoryService.create(categoryCreateDTO);
 
         if (result == null) {
-            var errorMessage = new Message("error", "Failed to create category");
-            model.addAttribute("message", errorMessage);
-            return "manage/categories/create";
+            return ResponseEntity.badRequest().body("Failed to create category");
         }
 
-        var successMessage = new Message("success", "Category created successfully");
-        redirectAttributes.addFlashAttribute("message", successMessage);
-        return "redirect:/manage/categories";
+        return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/edit/{id}")
-    public String edit(@PathVariable UUID id, Model model) {
-        var categoryDTO = categoryService.findById(id);
-        model.addAttribute("categoryDTO", categoryDTO);
-
-        return "manage/categories/edit";
-    }
-
-    @PostMapping("/edit/{id}")
-    public String edit(@PathVariable UUID id,
+    @PutMapping("/{id}")
+    @Operation(summary = "Edit category by id")
+    @ApiResponse(responseCode = "200", description = "Return updated category")
+    @ApiResponse(responseCode = "400", description = "Failed to update category")
+    public ResponseEntity<?> edit(@PathVariable UUID id,
             @ModelAttribute @Valid CategoryDTO categoryDTO,
-            RedirectAttributes redirectAttributes,
-            BindingResult bindingResult, Model model) {
+            BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("categoryDTO", categoryDTO);
-            return "manage/categories/edit";
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
 
         var result = categoryService.update(id, categoryDTO);
 
         if (result == null) {
-            var errorMessage = new Message("error", "Failed to update category");
-            model.addAttribute("message", errorMessage);
-            return "manage/categories/edit";
-
+            return ResponseEntity.badRequest().body("Failed to update category");
         }
 
-        var successMessage = new Message("success", "Category updated successfully");
-        redirectAttributes.addFlashAttribute("message", successMessage);
-        return "redirect:/manage/categories";
+        return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete category by id")
+    @ApiResponse(responseCode = "200", description = "Return true if delete success")
+    public ResponseEntity<Boolean> delete(@PathVariable UUID id) {
         var result = categoryService.deleteById(id);
 
-        if (!result) {
-            var errorMessage = new Message("error", "Failed to delete category");
-            redirectAttributes.addFlashAttribute("message", errorMessage);
-        } else {
-            var successMessage = new Message("success", "Category deleted successfully");
-            redirectAttributes.addFlashAttribute("message", successMessage);
-        }
-        return "redirect:/manage/categories";
+        return ResponseEntity.ok(result);
     }
 }
